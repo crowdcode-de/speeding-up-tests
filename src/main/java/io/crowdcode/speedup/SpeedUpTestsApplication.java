b172731f1dd8e91a84d85190abb04e6cc0dd1516
Ingo Düppe <ingo.dueppe@crowdcode.io>
2018-10-08 16:31:54 +0200

    add spring boot application

---
+++ src/main/java/io/crowdcode/speedup/SpeedUpTestsApplication.java
@@ -0,0 +1,12 @@
+package io.crowdcode.speedup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedUpTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedUpTestsApplication.class, args);
+    }
+}
