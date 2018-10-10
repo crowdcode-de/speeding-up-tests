9402385635b31b47b5bb40f61dd73a41a3bd34f7
Ingo Düppe <ingo.dueppe@crowdcode.io>
2018-10-10 13:01:26 +0200

    add scrumr inheritance example

---
+++ src/test/java/io/crowdcode/speedup/scrumr/service/inheritance/ScrumrServiceMockSprintRepositoryTest.java
@@ -0,0 +1,25 @@
+package io.crowdcode.speedup.scrumr.service.inheritance;
+
+import io.crowdcode.speedup.scrumr.service.ScrumrService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = {"spring.jpa.show-sql=true"})
+class ScrumrServiceMockSprintRepositoryTest extends AbstractScrumrMockBeanTest {
+
+    @Autowired
+    private ScrumrService taskService;
+
+    @Test
+    void testCreateTask() {
+        when(sprintRepository.count()).thenReturn(1L);
+        assertThat(taskService.createTask("TASK_X", 1)).isNotNull();
+    }
+}
